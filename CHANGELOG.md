06fea7edb69f4be8d48a5d5fccf38339ce8de7cf
Marcos Pérez García <mperez@idealista.com>
2021-04-06 18:12:20 +0200

    resolve #69 allow to store secret file credentials

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,9 @@ All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/) and [Keep a changelog](https://github.com/olivierlacan/keep-a-changelog).
 
 ## [Unreleased](https://github.com/idealista/jenkins_role/tree/develop)
+### Added
+- *[#69](https://github.com/idealista/jenkins_role/issues/69) [FEATURE] Allow to store secret file credentials* @emepege
+
 ## [2.7.0](https://github.com/idealista/jenkins_role/tree/2.7.0)
 ## [Full Changelog](https://github.com/idealista/jenkins_role/compare/2.6.0...2.7.0)
 ### Added
--- files/security/Credentials.groovy
+++ files/security/Credentials.groovy
@@ -4,10 +4,13 @@ import com.cloudbees.plugins.credentials.domains.Domain
 import com.cloudbees.plugins.credentials.impl.*
 import com.cloudbees.plugins.credentials.CredentialsScope
 import com.cloudbees.plugins.credentials.SystemCredentialsProvider
+import com.cloudbees.plugins.credentials.SecretBytes
 import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl
+import org.jenkinsci.plugins.plaincredentials.impl.FileCredentialsImpl
 import hudson.util.Secret
 import hudson.security.HudsonPrivateSecurityRealm
 import jenkins.model.Jenkins
+import java.nio.file.*
 
 def ansible = new Domain("ansible", "ansible managed credentials", null)
 SystemCredentialsProvider.getInstance().getStore().addDomain(ansible)
@@ -27,6 +30,14 @@ if ("${cred}" != "") {
         "${id}",
         "${description}",
         Secret.fromString("${text}"))
+} else if ("${file}" != "") {
+    Path fileLocation = Paths.get("${file}");
+    credential = new FileCredentialsImpl(
+        CredentialsScope.GLOBAL, 
+        "${id}", 
+        "${description}", 
+        fileLocation.getFileName().toString(), 
+        SecretBytes.fromBytes(Files.readAllBytes(fileLocation)))
 } else {
     credential = new UsernamePasswordCredentialsImpl(
         CredentialsScope.GLOBAL,
